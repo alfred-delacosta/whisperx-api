de92aee09f22aae8b1ef608e586899665ea0a769
Alfred De La Costa <alfred_delacosta@protonmail.com>
2026-03-02 18:10:01 -0500

    Got the convert video api endpoint about halfway there.

--- routers/ffmpeg.router.js
+++ routers/ffmpeg.router.js
@@ -1,5 +1,6 @@
 import { upload } from "../utils/multer.util.js";
 import express from "express";
+import { convertVideoToMp4 } from "../service/ffmpeg.service.js";
 
 const router = express.Router();
 
@@ -7,22 +8,27 @@ router.get("/", (req, res) => {
   res.send("You nailed it");
 });
 
-router.post("/convertVideo", upload.single("video"), (req, res) => {
+router.post("/convertVideo", upload.single("video"), async (req, res) => {
   try {
     if (!req.file) {
       return res.status(400).json({ error: "No video file provided" });
     }
 
-    const filePath = req.file.path;
+    const file = req.file;
+    const ffmpegProcess = await convertVideoToMp4(file, res);
 
-    res.status(200).json({
-      file,
+    ffmpegProcess.on("exit", () => {
+      res.status(200).json({
+        message: "Video converted successfully.",
+        file
+      });
+
     });
   } catch (error) {
     res.status(400).json({
-        message: "There was an error converting the video.",
-        error: error.message
-    })
+      message: "There was an error converting the video.",
+      error: error.message,
+    });
   }
 });
 
