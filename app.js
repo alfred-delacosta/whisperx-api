a911200c0d9e15ac3a33c5bef9902a8357f27fbc
Alfred De La Costa <alfred_delacosta@protonmail.com>
2026-08-15 13:50:33 -0400

    Let's try this.

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import 'dotenv/config'
 
+const THIRTY_MINUTES = 30 * 60 * 1000;
 
 const app = express();
 const port = process.env.PORT || 5052;
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  req.setTimeout(THIRTY_MINUTES);
+  res.setTimeout(THIRTY_MINUTES);
+  next();
+});
 
 //#region Routers
 import ffmpegRouter from './routers/ffmpeg.router.js';
@@ -21,6 +28,10 @@ app.use('/ffmpeg', ffmpegRouter);
 app.use('/whisperx', whisperXRouter)
 app.use('/subtitles', subtitlesRouter);
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 })
+
+server.requestTimeout = THIRTY_MINUTES;
+server.headersTimeout = THIRTY_MINUTES + 1000;
+server.keepAliveTimeout = THIRTY_MINUTES;
